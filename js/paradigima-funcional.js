db341587e9d042113a8f3754d8cccd484bcef08c
David <davidroliveira@gmail.com>
2020-11-22 18:56:53 -0400

    Implementação de paradigma funcional, tipo Any, typeOf, NaN (not a number)

---
+++ js/paradigima-funcional.js
@@ -0,0 +1,17 @@
+"use strict";
+function addNumTyped(num1, num2) {
+    return num1 + num2;
+}
+;
+function addNums(num1, num2) {
+    if (typeof (num1) == "string") {
+        if (isNaN(parseInt(num1, 10))) {
+            return 0;
+        }
+        num1 = parseInt(num1, 10);
+    }
+    return num1 + num2;
+}
+console.log(addNumTyped(10, 20));
+console.log(addNums('10', 20));
+//# sourceMappingURL=paradigima-funcional.js.map
---
+++ paradigima-funcional.ts
@@ -0,0 +1,18 @@
+function addNumTyped(num1: number, num2:number): number{
+    return num1 + num2;
+};
+
+function addNums(num1: any, num2: number){
+    if (typeof(num1) == "string") {
+        if (isNaN(parseInt(num1,10))) {
+            return 0;
+        }
+        num1 = parseInt(num1,10);
+    }
+    return num1 + num2;
+    
+}
+
+console.log(addNumTyped(10,20));
+
+console.log(addNums('10',20));
