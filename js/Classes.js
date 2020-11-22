360c7c2fa26c2e1943b0ecc0530f3f14f6da8717
David <davidroliveira@gmail.com>
2020-11-22 16:31:49 -0400

    Implementação de Classes e integração com Html document

---
+++ js/Classes.js
@@ -0,0 +1,20 @@
+"use strict";
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+;
+var greeter = new Greeter("world");
+var button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function () {
+    alert(greeter.greet());
+};
+document.body.appendChild(button);
+//console.log('david2');
+//# sourceMappingURL=Classes.js.map
