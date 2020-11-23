72af7c6456300a99a9e22a7556d10df7855f63f7
David <davidroliveira@gmail.com>
2020-11-22 20:07:15 -0400

    Implementações de tipos de variáveis

---
+++ Variaveis.ts
@@ -0,0 +1,11 @@
+//var -> var respeita escopo (NÃO É RECOMENDÁVEL!!!)
+var nome = 'Clayton';
+console.log(nome);
+
+//const -> valor não pode ser alterado
+const curso = 'Curso de Typescript';
+console.log(curso);
+
+//let -> let respeita escopo
+let url = 'http://medium.com/codigorefinado';
+console.log(url);
---
+++ js/Variaveis.js
@@ -0,0 +1,11 @@
+"use strict";
+//var -> var respeita escopo (NÃO É RECOMENDÁVEL!!!)
+var nome = 'Clayton';
+console.log(nome);
+//const -> valor não pode ser alterado
+var curso = 'Curso de Typescript';
+console.log(curso);
+//let -> let respeita escopo
+var url = 'http://medium.com/codigorefinado';
+console.log(url);
+//# sourceMappingURL=Variaveis.js.map
