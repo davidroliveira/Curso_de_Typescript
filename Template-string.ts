7e56bfc66b992b137ab02afba867b89f1ff29ca6
David <davidroliveira@gmail.com>
2020-11-24 21:01:46 -0400

    Template String

---
+++ Template-string.ts
@@ -0,0 +1,9 @@
+console.clear();
+
+var lyrics = 'Never gonna give up';
+var html = `<div>${lyrics}<div>`;
+
+console.log(html);
+
+
+console.log(`1 and 1 make ${1+1}`);
---
+++ js/Template-string.js
@@ -0,0 +1,7 @@
+"use strict";
+console.clear();
+var lyrics = 'Never gonna give up';
+var html = "<div>" + lyrics + "<div>";
+console.log(html);
+console.log("1 and 1 make " + (1 + 1));
+//# sourceMappingURL=Template-string.js.map
